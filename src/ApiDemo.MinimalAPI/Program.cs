325bac95d165276b66060c33d9776d512fc45978
Robin La Rondelle <robinlarondelle@hotmail.com>
2021-11-11 17:30:37 +0100

    Removed comments

--- src/ApiDemo.MinimalAPI/Program.cs
+++ src/ApiDemo.MinimalAPI/Program.cs
@@ -3,13 +3,10 @@ using ApiDemo.MinimalAPI.Utils;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Configure the services
 builder.Services.AddEndpointDefinition(typeof(Customer));
 
 var app = builder.Build();
 
-// Configure endpoints and middleware
 app.UseEndpointDefinitions();
 
-// Run the application
 app.Run();
